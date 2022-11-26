b4f8431d150170d4333ac644d2560dfe26b199d4
student079 <102852555+student079@users.noreply.github.com>
2022-11-26 23:34:20 +0900

    .

---
+++ 최소공배수_백준1934.py
@@ -0,0 +1,17 @@
+# 유클리드 호제법 사용
+import sys
+# 재귀 깊이 조절
+sys.setrecursionlimit(10**6)
+
+def gcd(a, b):
+    r = a % b
+    if r != 0 : 
+        return gcd(b,r)
+    else:
+        return b
+n = int(sys.stdin.readline().rstrip('\n'))
+for i in range(n):
+    a,b = map(int, sys.stdin.readline().rstrip('\n').split())
+    minn = gcd(a,b)
+    maxn = int(a*b/minn)
+    print(maxn)
