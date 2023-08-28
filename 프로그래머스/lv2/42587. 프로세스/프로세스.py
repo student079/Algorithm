e94e2228bd1e7ef75faf9b494878e75329f158bc
student079 <102852555+student079@users.noreply.github.com>
2023-08-28 16:27:42 +0900

    [level 2] Title: 프로세스, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -1,31 +1,20 @@
+from collections import deque
 def solution(priorities, location):
-    # location 인덱스에 있는 priority값 저장해놓고 -1로 변경
-    target = priorities[location]
-    priorities[location] = -1
     
-    # priorities max랑 비교하면서 max값이 priority값이랑 비교
-    count = 1
-    while priorities:
-        
-    # priority보다 크면 그때 count
-        pMax = max(priorities)
-        if target > pMax:
-            return count
-        
-    # priority보다 작으면 시뮬레이션 진행    
-        elif target < pMax:
-            while priorities:
-                if priorities[0] == pMax:
-                    priorities.pop(0)
-                    count += 1
-                    break
-                else :
-                    priorities.append(priorities.pop(0))
+    #enumerate, deque 사용해서 index, value 쌍으로 저장
+    queue = deque([(i,p) for i, p in enumerate(priorities)])
     
-    # 같으면 앞에인덱스 중 max값과 같은 개수 만큼 count 증가
+    count = 0
+    while queue:
+        currentIndex, currentValue = queue.popleft()
+        
+        # priority가 있는거보다 작으면 다시 큐에 넣기
+        if any(currentValue < priority for _, priority in queue):
+            queue.append((currentIndex, currentValue))
+        # 크거나 같으면 count 증가시키고 location과 비교해서 같으면 count 리턴
         else:
-            for i in priorities:
-                if i == -1:
-                    return count
-                elif i == target:
-                    count += 1
+            count += 1
+            if location == currentIndex:
+                return count
+            
+    return count
