e6a261d39ad4fd7f753336507d8520de3a4afe1d
student079 <102852555+student079@users.noreply.github.com>
2023-08-29 11:47:28 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,24 +1,25 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    
-    stack = deque()
     truck_weights = deque(truck_weights)
-    count = 0
+    on_bridge = deque()  # 현재 다리 위의 트럭
+    time = 0
+    total_weight = 0
+    last_entered_time = 0
     
     # while문 돌면서 count 증가
-    while truck_weights or stack:
-        count+=1 # time
+    while truck_weights or on_bridge:
+        time += 1
         
         # 나갈 트럭
-        if any(count - i[1] >= bridge_length for i in stack):
-            stack.popleft()
+        if on_bridge and time - on_bridge[0][1] >= bridge_length:
+            truck, entered_time = on_bridge.popleft()
+            total_weight -= truck
         
         # length랑 sum체크해서 더들어갈 수 있으면
-        if truck_weights and sum(i[0] for i in stack)+truck_weights[0] <= weight and\
-        len(stack) + 1 <= bridge_length:
-            # 튜플 형태로 넣기
-            stack.append((truck_weights.popleft(), count))
-        
-            
-    return count
+        if truck_weights and total_weight + truck_weights[0] <= weight and len(on_bridge) + 1 <= bridge_length:
+            truck = truck_weights.popleft()
+            total_weight += truck
+            on_bridge.append((truck, time)) # 튜플 형태로 넣기
+    
+    return time
