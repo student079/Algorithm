389cdac38f65ca5d107fc69090253a1c14c713e7
student079 <102852555+student079@users.noreply.github.com>
2023-09-06 14:12:32 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,33 @@
+from collections import deque
+import heapq
+
+def solution(jobs):
+    
+    # 후보들 중에 걸리는 시간이 짧은 거 먼저하면 될듯
+    currentTime = 0
+    answer = 0
+    n = len(jobs)
+    
+    jobs.sort()
+    jobs = deque(jobs)
+    heap = []
+    heapq.heapify(heap)
+    
+    while True:
+        
+        # 현재 시간이랑 비교해서 이전인 것들 힙에 넣기
+        while jobs and jobs[0][0] <= currentTime:
+            start, workTime = jobs.popleft()
+            heapq.heappush(heap, (workTime, start))
+        
+        if heap:
+            workTime, start = heapq.heappop(heap)
+            currentTime += workTime
+            answer += currentTime - start # 소요시간 기록
+        else :
+            currentTime += 1
+        
+        if not jobs and not heap:
+            break
+            
+    return answer//n
