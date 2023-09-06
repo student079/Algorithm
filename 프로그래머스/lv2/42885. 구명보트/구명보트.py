9b5a6daee6dcba927b80e051467b92d97c3fb41f
student079 <102852555+student079@users.noreply.github.com>
2023-09-06 12:42:49 +0900

    [level 2] Title: 구명보트, Time: 8.30 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,17 @@
+from collections import deque
+def solution(people, limit):
+    
+    cnt = 0
+    people.sort()
+    people = deque(people)
+    #left, right = 0, len(people) -1
+    
+    cLimit = limit
+    while people:
+        cnt += 1
+        cLimit -= people.pop()
+        if people and cLimit >= people[0]:
+            people.popleft()
+        cLimit = limit
+    
+    return cnt
