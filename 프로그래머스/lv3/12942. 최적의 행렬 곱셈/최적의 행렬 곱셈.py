45c05bdc20d90a7e2dae2a75d5540a39c25a192e
student079 <102852555+student079@users.noreply.github.com>
2023-09-23 13:49:52 +0900

    [level 3] Title: 최적의 행렬 곱셈, Time: 466.65 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.py
+++ 프로그래머스/lv3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.py
@@ -1,20 +1,18 @@
 # 다시 풀어보기
 
+import sys
 def solution(matrix_sizes):
-    sizes = len(matrix_sizes)
-    dp = [[0 for j in range(sizes)] for i in range(sizes)]
-    
-    for gap in range(1, sizes) : 
-        for s in range(0, sizes-gap) : 
-            e = s+gap
-            
-            candidate = list()
-            for m in range(s, e) :
-                candidate.append(
-                    dp[s][m]+dp[m+1][e]+
-                    matrix_sizes[s][0]*matrix_sizes[m][1]*matrix_sizes[e][1])
-            dp[s][e] = min(candidate)
-    
-    print(dp)
-            
-    return dp[0][-1]
+    answer = 0
+    d = [matrix_sizes[0][0]]
+    for i in range(len(matrix_sizes)):
+        d.append(matrix_sizes[i][1])
+    length = len(d)
+    M = [[0 for x in range(length)] for y in range(length)]
+    for diag in range(1, length):
+        for i in range(1, length-diag):
+            j = i+diag
+            M[i][j] = sys.maxsize
+            for k in range(i,j):
+                M[i][j] = min(M[i][j], M[i][k] + M[k+1][j] + d[i-1]*d[k]*d[j])
+    answer = M[1][length-1]
+    return answer
