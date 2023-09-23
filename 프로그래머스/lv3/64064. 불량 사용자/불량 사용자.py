adf7c48699400acfb8b9d8098c9153c0e714cce1
student079 <102852555+student079@users.noreply.github.com>
2023-09-23 13:27:06 +0900

    [level 3] Title: 불량 사용자, Time: 2.53 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -1,28 +1,31 @@
+# 다시 풀어보기
+
 from itertools import permutations
 
 def solution(user_id, banned_id):
-    answer = set()
     
+    answer = set()
+
     # 일단 경우의 수 다 뽑고
     b_len = len(banned_id)
     user_permutations = list(permutations(user_id, b_len))
-    
+
     def check(users, banned):
         for i in range(b_len):
             l = len(users[i])
             if l != len(banned[i]):
                 return False
-            
+
             for j in range(l):
                 if banned[i][j] == '*':
                     continue
                 if users[i][j] != banned[i][j]:
                     return False
         return True
-            
-    
+
+
     for users in user_permutations:
         if check(users,banned_id):
             answer.add(tuple(sorted(users)))
-    
+
     return len(answer)
