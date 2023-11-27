3ba610318b0d2e135687076af82bfb8681ec0fbd
student079 <102852555+student079@users.noreply.github.com>
2023-11-27 15:07:57 +0900

    [level 5] Title: 방의 개수, Time: 498.13 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+def solution(arrows):
+    answer = 0
+    
+    # 뭔가 2차원배열 쓰는 건 아닌것 같고
+    # 대각선 교차도 포함 -> 2칸 씩 이동
+    # 방문했던 거 또 방문에다가 처음 경로면 방 +
+    # 경로랑 정점 저장
+    
+    visited = defaultdict(list)
+    dx = (-1,-1,0,1,1,1,0,-1)
+    dy = (0,1,1,1,0,-1,-1,-1)
+    x,y = 0,0
+    for arrow in arrows:
+        for _ in range(2):
+            nx = x+dx[arrow]
+            ny = y+dy[arrow]
+            
+            # 이 정점은 방문했었지만 이 경로로 방문했던 적 없음
+            if (nx,ny) in visited and (x,y) not in visited[(nx,ny)]:
+                answer+=1
+                visited[(nx,ny)].append((x,y))
+                visited[(x,y)].append((nx,ny))
+            elif (nx,ny) not in visited:
+                visited[(nx,ny)].append((x,y))
+                visited[(x,y)].append((nx,ny))
+            
+            x = nx
+            y = ny
+    
+    
+    return answer
