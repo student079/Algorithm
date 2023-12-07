47df89dcaf212a666d0222e2a25347038d3a91df
student079 <102852555+student079@users.noreply.github.com>
2023-12-07 17:41:25 +0900

    [level 4] Title: 도둑질, Time: 628.53 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,18 +1,25 @@
 def solution(money):
     
-    # 딱봐도 DP
+    # dp
+    # 원이기 때문에 첫번째 집 털면 마지막 집 못터니까
+    # 두개로 나눠
+    # dp[0] : 첫번째 집 터는 경우
+    # dp[1] : 첫번째 집 안터는 경우
+    l = len(money)
+    dp = [[0] * l for _ in range(2)]
     
-    # 첫번째 집 털경우
-    dp = [0] * len(money)
-    dp[0] = money[0]
-    dp[1] = max(money[0], money[1])
-    for i in range(2, len(money) - 1):
-        dp[i] = max(dp[i-1], dp[i-2] + money[i])
+    # 초기화
+    # dp[0]
+    dp[0][0] = money[0]
+    dp[0][1] = max(money[0],money[1])
+    # dp[1]
+    dp[1][1] = money[1]
     
-    # 첫번째 집 안털고 마지막 집 털경우
-    dp2 = [0] * len(money)
-    dp2[1] =  money[1]
-    for i in range(2,len(money)):
-        dp2[i] = max(dp2[i-1], dp2[i-2] + money[i])
+    for j in range(2):
+        for i in range(2, l-1):
+            dp[j][i] = max(dp[j][i-1], dp[j][i-2] + money[i])
     
-    return max(max(dp), max(dp2))
+    # 마지막 집 추가
+    dp[1][l-1] = max(dp[1][l-2], dp[1][l-3] + money[l-1])
+        
+    return max(max(dp[0]), max(dp[1]))
