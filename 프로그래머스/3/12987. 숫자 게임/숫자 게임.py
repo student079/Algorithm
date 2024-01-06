f4f7d1edd1936191f13269b53f261e56228de6a8
student079 <102852555+student079@users.noreply.github.com>
2024-01-06 15:40:41 +0900

    [level 3] Title: 숫자 게임, Time: 40.93 ms, Memory: 18.3 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -1,20 +1,14 @@
 def solution(A, B):
     
-    # 대충 스택쓰는 느낌인데
-    # B 정렬해서
-    # 하나씩 확인하고 빼는 느낌으로 가자
     answer = 0
     A.sort()
     B.sort()
     N = len(A)
+    
     a = 0
-    b = 0
-    for _ in range(N):
+    for b in range(N):
         if A[a] < B[b]:
             answer+=1
-            b+=1
             a+=1
-        else:
-            b+=1
             
     return answer
