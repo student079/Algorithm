0c74e3dd05ea54f1219c48c615ee7d985a715ad2
student079 <102852555+student079@users.noreply.github.com>
2024-02-01 14:28:36 +0900

    [Gold V] Title: A와 B 2, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -0,0 +1,25 @@
+import sys
+
+S = list(sys.stdin.readline().rstrip())
+T = list(sys.stdin.readline().rstrip())
+
+# S 1 , T 50 일때 2**50  -> X
+answer = 0
+N = len(S)
+
+def check(str):
+    global answer
+    if str == S:
+        answer = 1
+        return
+    if len(str) < N:
+        return
+    
+    if str[0] == 'B':
+        check(str[1:][::-1])
+    if str[-1] == 'A':
+        check(str[:-1])
+
+check(T)
+print(answer)
+
