05acdab2708a662117980100a047dcfe4a1ddbcb
student079 <102852555+student079@users.noreply.github.com>
2024-04-06 18:03:01 +0900

    [Gold II] Title: 2048 (Easy), Time: 348 ms, Memory: 122628 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -1,122 +1,148 @@
+# 상하좌우
+# 블록이 이미 합쳐졌는지 체크해야해
+# 5번 가장 큰 블록
+from collections import deque
+
 import sys
-from copy import deepcopy
 
 N = int(sys.stdin.readline())
 board = []
-for _ in range(N):
-    board.append(list(map(int,sys.stdin.readline().split())))
+maxBlock = 0
+for i in range(N):
+    board.append(list(map(int,sys.stdin.readline().rstrip().split())))
+    maxBlock = max(maxBlock, max(board[i]))
+dx = (1, 0, -1, 0)
+dy = (0 ,1, 0, -1)
+# 하, 우, 상, 좌
 
-# 상하좌우의 네가지가 5번 4^5
-# dfs로 가능해
-# 위쪽으로 밀면 위에서 두번째부터 그위랑 비교해서 합치기
-# 보드 deepcopy하기
-# target 변수를 만들어서 합쳐지면 다른것을 가리키게끔 해서
-# 중복 방지
+q = deque()
+q.append((board, 0))
+while q:
+    oriBoard, cnt = q.popleft()
+    if cnt >= 5:
+        continue
 
-def up(board):
-    for j in range(N):
-        # 열 하나씩 확인
-        target = 0
-        for i in range(1,N):
-            if board[i][j] != 0:
-                temp = board[i][j]
-                board[i][j] = 0
+    for k in range(4):
+        # 합쳐진 블록인지 체크
+        visited = [[False] * N for _ in range(N)]
+        newBoard = []
+        for i in range(N):
+            newBoard.append(oriBoard[i][:])
 
-                # 타켓이 0 일때
-                if board[target][j] == 0:
-                    board[target][j] = temp
-                # 타겟이 현재 값과 같을 때 제곱하고 타겟 한칸 내리기
-                elif board[target][j] == temp:
-                    board[target][j] = temp*2
-                    target+=1
-                # 그 외라면 타겟 한칸내리고 그자리에 현재 값 넣기
-                else:
-                    target+=1
-                    board[target][j] = temp
-    return board
+        # 하
+        if k == 0:
+            # N -2 줄 부터 0까지
+            for j in range(N):
+                for i in range(N-2, -1, -1):
+                    if newBoard[i][j] == 0:
+                        continue
+                    ni = i
+                    nj = j
+                    while True:
+                        ni += dx[k]
+                        nj += dy[k]
+                        if not ( 0 <= ni < N and 0 <= nj < N):
+                            break
+                        if newBoard[ni][nj] == 0:
+                            newBoard[ni][nj] = newBoard[ni-dx[k]][nj-dy[k]]
+                            newBoard[ni - dx[k]][nj - dy[k]] = 0
+                            i = ni
+                            j = nj
+                        else:
+                            break
+                    if (0 <= ni < N and 0 <= nj < N) and \
+                        newBoard[ni][nj] == newBoard[i][j] and not visited[ni][nj]:
+                        newBoard[i][j] = 0
+                        newBoard[ni][nj] *= 2
+                        maxBlock = max(newBoard[ni][nj], maxBlock)
+                        visited[ni][nj] = True
+            q.append((newBoard, cnt + 1))
 
-def down(board):
-    for j in range(N):
-        # 열 하나씩 확인
-        target = N-1
-        for i in range(N-2,-1,-1):
-            if board[i][j] != 0:
-                temp = board[i][j]
-                board[i][j] = 0
+        # 우
+        elif k == 1:
+            # N -2 줄 부터 0까지
+            for i in range(N):
+                for j in range(N - 2, -1, -1):
+                    if newBoard[i][j] == 0:
+                        continue
+                    ni = i
+                    nj = j
+                    while True:
+                        ni += dx[k]
+                        nj += dy[k]
+                        if not (0 <= ni < N and 0 <= nj < N):
+                            break
+                        if newBoard[ni][nj] == 0:
+                            newBoard[ni][nj] = newBoard[ni - dx[k]][nj - dy[k]]
+                            newBoard[ni - dx[k]][nj - dy[k]] = 0
+                            i = ni
+                            j = nj
+                        else:
+                            break
+                    if (0 <= ni < N and 0 <= nj < N) and newBoard[ni][nj] == newBoard[i][j] and not visited[ni][nj]:
+                        newBoard[i][j] = 0
+                        newBoard[ni][nj] *= 2
+                        maxBlock = max(newBoard[ni][nj], maxBlock)
+                        visited[ni][nj] = True
+            q.append((newBoard, cnt + 1))
 
-                # 타켓이 0 일때
-                if board[target][j] == 0:
-                    board[target][j] = temp
-                # 타겟이 현재 값과 같을 때 제곱하고 타겟 한칸 올리기
-                elif board[target][j] == temp:
-                    board[target][j] = temp*2
-                    target-=1
-                # 그 외라면 타겟 한칸올리고 그자리에 현재 값 넣기
-                else:
-                    target-=1
-                    board[target][j] = temp
-        
-    return board
+        # 상
+        elif k == 2:
+            # N -2 줄 부터 0까지
+            for j in range(N):
+                for i in range(1, N):
+                    if newBoard[i][j] == 0:
+                        continue
+                    ni = i
+                    nj = j
+                    while True:
+                        ni += dx[k]
+                        nj += dy[k]
+                        if not (0 <= ni < N and 0 <= nj < N):
+                            break
+                        if newBoard[ni][nj] == 0:
+                            newBoard[ni][nj] = newBoard[ni - dx[k]][nj - dy[k]]
+                            newBoard[ni - dx[k]][nj - dy[k]] = 0
+                            i = ni
+                            j = nj
+                        else:
+                            break
+                    if (0 <= ni < N and 0 <= nj < N) and \
+                            newBoard[ni][nj] == newBoard[i][j] and not visited[ni][nj]:
+                        newBoard[i][j] = 0
+                        newBoard[ni][nj] *= 2
+                        maxBlock = max(newBoard[ni][nj], maxBlock)
+                        visited[ni][nj] = True
+            q.append((newBoard, cnt + 1))
 
-def right(board):
-    for i in range(N):
-        # 행 하나씩 확인
-        target = N-1
-        for j in range(N-2,-1,-1):
-            if board[i][j] != 0:
-                temp = board[i][j]
-                board[i][j] = 0
+        # 좌
+        else:
+            # N -2 줄 부터 0까지
+            for i in range(N):
+                for j in range(1,N):
+                    if newBoard[i][j] == 0:
+                        continue
+                    ni = i
+                    nj = j
+                    while True:
+                        ni += dx[k]
+                        nj += dy[k]
+                        if not (0 <= ni < N and 0 <= nj < N):
+                            break
+                        if newBoard[ni][nj] == 0:
+                            newBoard[ni][nj] = newBoard[ni - dx[k]][nj - dy[k]]
+                            newBoard[ni - dx[k]][nj - dy[k]] = 0
+                            i = ni
+                            j = nj
+                        else:
+                            break
+                    if (0 <= ni < N and 0 <= nj < N) and \
+                            newBoard[ni][nj] == newBoard[i][j] and not visited[ni][nj]:
+                        newBoard[i][j] = 0
+                        newBoard[ni][nj] *= 2
+                        maxBlock = max(newBoard[ni][nj], maxBlock)
+                        visited[ni][nj] = True
+            q.append((newBoard, cnt + 1))
 
-                # 타켓이 0 일때
-                if board[i][target] == 0:
-                    board[i][target] = temp
-                # 타겟이 현재 값과 같을 때 제곱하고 타겟 한칸 왼쪽
-                elif board[i][target] == temp:
-                    board[i][target] = temp*2
-                    target-=1
-                # 그 외라면 타겟 왼쪽 그자리에 현재 값 넣기
-                else:
-                    target-=1
-                    board[i][target] = temp
-        
-    return board
-
-def left(board):
-    for i in range(N):
-        # 행 하나씩 확인
-        target = 0
-        for j in range(1,N):
-            if board[i][j] != 0:
-                temp = board[i][j]
-                board[i][j] = 0
-
-                # 타켓이 0 일때
-                if board[i][target] == 0:
-                    board[i][target] = temp
-                # 타겟이 현재 값과 같을 때 제곱하고 타겟 한칸 왼쪽
-                elif board[i][target] == temp:
-                    board[i][target] = temp*2
-                    target+=1
-                # 그 외라면 타겟 왼쪽 그자리에 현재 값 넣기
-                else:
-                    target+=1
-                    board[i][target] = temp
-
-    return board
-             
-ans = 0
-
-def dfs(board,cnt):
-    global ans
-
-    if cnt == 5:
-        ans = max(ans, max(map(max, board)))
-        return
-    
-    dfs(up(deepcopy(board)),cnt+1)
-    dfs(down(deepcopy(board)),cnt+1)
-    dfs(right(deepcopy(board)),cnt+1)
-    dfs(left(deepcopy(board)),cnt+1)
-
-dfs(board,0)
-print(ans)
+# 블록 최대값 확인
+print(maxBlock)
