7cbe7b163cfd4b24099ac6585344c5d79c6036ff
student079 <102852555+student079@users.noreply.github.com>
2024-04-16 16:07:22 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 48 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,48 @@
+# 그래프 
+# 거리
+# 모든 노드의 최단 거리
+
+# N 2 ~ 100, M 1 ~ 5000
+
+import sys
+import heapq
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 다익스트라 N 번 돌리기?
+# 시간상으로는 가능
+
+def dijkstra(start) -> int:
+    temp = [float('inf')] * (N+1)
+    
+    q = []
+    temp[start] = 0
+    q.append((0, start))
+
+    while q:
+        dis, node = heapq.heappop(q)
+        
+        for toNode in graph[node]:
+            if temp[toNode] > dis + 1:
+                temp[toNode] = dis + 1
+                q.append((dis+1, toNode))
+
+    return sum(temp[1:])
+
+
+res = float('inf')
+resIdx = -1
+
+for i in range(1, N+1):
+    temp = dijkstra(i)
+    if temp < res:
+        res = temp
+        resIdx = i
+
+print(resIdx)
