9ff265e40b0e33761da19e0c9da153494f85e79a
student079 <102852555+student079@users.noreply.github.com>
2024-04-23 16:51:04 +0900

    [level 2] Title: 주식가격, Time: 25.64 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,25 @@
+// 스택으로
+// 더 작은 값은 넣기
+
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < prices.length ; i++ ) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                answer[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        
+        while (!stack.isEmpty()) {
+            answer[stack.peek()] = prices.length - 1 - stack.pop();
+        }
+        
+        return answer;
+    }
+}
