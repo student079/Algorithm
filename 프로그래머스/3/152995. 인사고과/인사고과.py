ea3f5fc32377f2a8d70e711ce25cb9d7793649d1
student079 <102852555+student079@users.noreply.github.com>
2024-05-02 18:36:17 +0900

    [level 3] Title: 인사고과, Time: 146.85 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/152995. 인사고과/인사고과.py
@@ -0,0 +1,20 @@
+def solution(scores):
+    answer = 0
+    target_a, target_b = scores[0]
+    target_score = target_a + target_b
+
+    # 첫번째 점수에 대해서 내림차순,
+    # 첫 번째 점수가 같으면 두 번째 점수에 대해서 오름차순으로 정렬합니다.
+    scores.sort(key=lambda x: (-x[0], x[1]))
+    maxb = 0
+    
+    for a, b in scores:
+        if target_a < a and target_b < b:
+            return -1
+        
+        if b >= maxb:
+            maxb = b
+            if a + b > target_score:
+                answer += 1
+            
+    return answer + 1
