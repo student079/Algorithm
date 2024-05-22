56ce9243cf9797b268b57c0f0cb5f69b9fe69d17
student079 <102852555+student079@users.noreply.github.com>
2024-05-22 15:54:10 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,26 @@
+def solution(name):
+    
+    #하나씩 돌면서 상하로 변경하는거 더해주고
+    # 연속된 A개수 따라서 최소 이동하는 횟수 마지막에 더해주기
+    
+	# 조이스틱 조작 횟수 
+    answer = 0
+    
+    # 기본 최소 좌우이동 횟수는 길이 - 1
+    min_move = len(name) - 1
+    
+    for i, char in enumerate(name):
+    	# 해당 알파벳 변경 최솟값 추가
+        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+        
+        # 해당 알파벳 다음부터 연속된 A 문자열 찾기
+        next = i + 1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+            
+        # 기존, 연속된 A의 왼쪽시작 방식, 연속된 A의 오른쪽시작 방식 비교 및 갱신
+        min_move = min([min_move, 2 *i + len(name) - next, i + 2 * (len(name) -next)])
+
+    # 알파벳 변경(상하이동) 횟수에 좌우이동 횟수 추가
+    answer += min_move
+    return answer
