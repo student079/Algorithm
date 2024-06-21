57bec4e026f8d64950c62f94d20970b356a41894
student079 <102852555+student079@users.noreply.github.com>
2024-06-21 17:17:50 +0900

    [Silver II] Title: 너구리 구구, Time: 4 ms, Memory: 2444 KB -BaekjoonHub

--- 백준/Silver/18126. 너구리 구구/너구리 구구.cc
+++ 백준/Silver/18126. 너구리 구구/너구리 구구.cc
@@ -1,73 +1,71 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <queue>
-#include <algorithm>
-#define MAX 5001
-#define INF 5e12+1
-#define LL long long
-#define FASTIO cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+
+// 입구에서 가장 먼 방 구하기
+// N 5000
+// BFS 5000*log(5000)
+// C 10억 long long
 
 using namespace std;
+#include <iostream>
+#include <vector>
+#include <queue>
+#define FASTIO cin.tie(0); ios::sync_with_stdio(0);
+#define MAX 5001
 int N, A, B;
-LL C;
-vector<pair<int, LL> > Edge[MAX];
-LL Dist[MAX];
-LL Answer;
+long long C;
+long long answer;
+vector<pair<int, long long>> graph[MAX];
+long long dist[MAX];
 
-void init() {
-    for (int i = 0; i < MAX; i++) {
-        Dist[i] = INF;
-    }
-}
 
 void input() {
-    cin >> N;
-    for (int i = 0; i < (N - 1); i++) {
-        cin >> A >> B >> C;
-        Edge[A].push_back(make_pair(B, C));
-        Edge[B].push_back(make_pair(A, C));
-    }
+	cin >> N;
+	for (int i = 0; i < N - 1; i++) {
+		cin >> A >> B >> C;
+		graph[A].push_back(make_pair(B, C));
+		graph[B].push_back(make_pair(A, C));
+	}
+}
+
+void init() {
+	for (int i = 0; i < MAX; i++) {
+		dist[i] = 5e12 + 1;
+	}
 }
 
 void BFS() {
-    Dist[1] = 0;
-    queue<pair<int, LL> > Q;
-    Q.push(make_pair(1, 0));
+	dist[1] = 0;
+	queue<pair<int, long long>> q;
+	q.push(make_pair(1, 0));
 
-    while (!Q.empty()) {
-        int NowX = Q.front().first;
-        LL NowC = Q.front().second;
-        Q.pop();
+	while (!q.empty()) {
+		int node = q.front().first;
+		long long d = q.front().second;
+		q.pop();
 
-        for (int i = 0; i < Edge[NowX].size(); i++) {
-            int NextX = Edge[NowX][i].first;
-            LL NextC = Edge[NowX][i].second;
-            if (Dist[NextX] > NowC + NextC) {
-                Dist[NextX] = NowC + NextC;
-                Q.push(make_pair(NextX, Dist[NextX]));
-            }
-        }
-    };
+		for (int i = 0; i < graph[node].size(); i++) {
+			int nextNode = graph[node][i].first;
+			long long nextD = graph[node][i].second;
+			if (dist[nextNode] > d + nextD) {
+				dist[nextNode] = d + nextD;
+				q.push(make_pair(nextNode, dist[nextNode]));
+			}
+		}
+	}
 }
 
-void settings() {
-    BFS();
-    for (int i = 1; i <= N; i++) {
-        Answer = max(Answer, Dist[i]);
-    }
+void getAnswer() {
+	for (int i = 1; i <= N; i++) {
+		answer = max(answer, dist[i]);
+	}
+	cout << answer << "\n" ;
 }
 
-void find_Answer() {
-    cout << Answer << "\n";
-}
+int main(void) {
+	FASTIO
+	input();
+	init();
+	BFS();
+	getAnswer();
 
-int main() {
-    FASTIO
-    init();
-    input();
-    settings();
-    find_Answer();
-
-    return 0;
+	return 0;
 }
