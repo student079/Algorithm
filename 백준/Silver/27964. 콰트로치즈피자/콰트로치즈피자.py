097d2d1a25dcf16f3756efe16450be616d4cccb0
student079 <102852555+student079@users.noreply.github.com>
2024-07-11 19:24:43 +0900

    [Silver V] Title: 콰트로치즈피자, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/27964. 콰트로치즈피자/콰트로치즈피자.py
+++ 백준/Silver/27964. 콰트로치즈피자/콰트로치즈피자.py
@@ -6,8 +6,7 @@ N = int(sys.stdin.readline())
 cnt = 0
 for item in set(sys.stdin.readline().rstrip().split()):
     # Cheese로 끝나는 것
-    l = len(item)
-    if l >= 6 and item[l-6:l] == "Cheese":
+    if item.endswith("Cheese"):
         cnt+=1
         if cnt >= 4:
             print("yummy")
